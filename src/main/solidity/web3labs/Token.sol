0dfdf338e4d016be35329b24f1db9b4263428563
rachid chami <chamirachid1@gmail.com>
2021-01-07 16:15:20 +0100

    update Token.sol and remove unnecessary library

--- src/main/solidity/web3labs/Token.sol
+++ src/main/solidity/web3labs/Token.sol
@@ -1,18 +1,16 @@
-pragma solidity ^0.5.2;
+pragma solidity ^0.7.0;
 
-import "../openzeppelin/token/ERC20/ERC20.sol";
-import "../openzeppelin/token/ERC20/ERC20Burnable.sol";
-import "../openzeppelin/token/ERC20/ERC20Detailed.sol";
-import "../openzeppelin/token/ERC20/ERC20Mintable.sol";
-import "../openzeppelin/token/ERC20/ERC20Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 
-contract Token is ERC20, ERC20Burnable, ERC20Detailed, ERC20Mintable, ERC20Pausable {
+contract Token is ERC20, ERC20Burnable {
 
     constructor(
         uint totalSupply,
-        string memory name, string memory symbol, uint8 decimals)
-        public ERC20Detailed(name, symbol, decimals) {
-
+        string memory name,
+        string memory symbol,
+        uint8 decimals) ERC20(name, symbol) {
+        _setupDecimals(decimals);
         _mint(msg.sender, totalSupply);
     }
 }
